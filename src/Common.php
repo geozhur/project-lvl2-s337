763095a9d85a756717d5fc761f42c651698bd38f
Santa Klaus <egorich2004@mail.ru>
2018-10-02 18:17:51 +0200

    fix function genDiff

--- src/Common.php
+++ src/Common.php
@@ -15,61 +15,53 @@ function encode($data)
     }
 }
 
+function toStr($spaces, $elem, $key, $value)
+{
+    return sprintf("%{$spaces}s %s: %s", $elem, $key, $value);
+}
+
 function genDiff($content1, $content2, $level)
 {
     $contentArr1 = get_object_vars($content1);
     $contentKeys1 = array_keys($contentArr1);
     $contentArr2 = get_object_vars($content2);
     $contentKeys2 = array_keys($contentArr2);
-    $spases = $level*4-1;
-    $result1 = Collection\flattenAll(array_map(function ($item) use ($contentArr1, $contentArr2, $level, $spases) {
-
+    $spaces = $level*4-1;
+    $result1 = Collection\flattenAll(array_map(function ($item) use ($contentArr1, $contentArr2, $level, $spaces) {
+        $value1 = $contentArr1[$item];
         if (array_key_exists($item, $contentArr2)) {
-            if (is_object($contentArr1[$item])) {
-                return sprintf(
-                    "%{$spases}s %s: %s",
-                    '',
-                    $item,
-                    genDiff($contentArr1[$item], $contentArr2[$item], $level+1)
-                );
+            $value2 = $contentArr2[$item];
+            if (is_object($value1)) {
+                return toStr($spaces, '', $item, genDiff($value1, $value2, $level+1));
             } else {
-                if ($contentArr1[$item] == $contentArr2[$item]) {
-                        return sprintf("%{$spases}s %s: %s", '', $item, encode($contentArr1[$item]));
+                if ($value1 == $value2) {
+                        return toStr($spaces, '', $item, encode($value1));
                 } else {
-                        return [sprintf("%{$spases}s %s: %s", '-', $item, encode($contentArr1[$item])),
-                                sprintf("%{$spases}s %s: %s", '+', $item, encode($contentArr2[$item]))];
+                        return [toStr($spaces, '-', $item, encode($value1)),
+                                toStr($spaces, '+', $item, encode($value2))];
                 }
             }
         } else {
-            if (!is_object($contentArr1[$item])) {
-                return sprintf("%{$spases}s %s: %s", '-', $item, encode($contentArr1[$item]));
+            if (!is_object($value1)) {
+                return toStr($spaces, '-', $item, encode($value1));
             } else {
-                return sprintf(
-                    "%{$spases}s %s: %s",
-                    '-',
-                    $item,
-                    genDiff($contentArr1[$item], $contentArr1[$item], $level+1)
-                );
+                return toStr($spaces, '-', $item, genDiff($value1, $value1, $level+1));
             }
         }
     }, $contentKeys1));
 
-    $result2 = array_map(function ($item) use ($contentArr2, $spases, $level) {
-        if (!is_object($contentArr2[$item])) {
-            return sprintf("%{$spases}s %s: %s", '+', $item, encode($contentArr2[$item]));
+    $result2 = array_map(function ($item) use ($contentArr2, $spaces, $level) {
+        $value3 = $contentArr2[$item];
+        if (!is_object($value3)) {
+            return toStr($spaces, '+', $item, encode($value3));
         } else {
-            return sprintf(
-                "%{$spases}s %s: %s",
-                '+',
-                $item,
-                genDiff($contentArr2[$item], $contentArr2[$item], $level+1)
-            );
+            return toStr($spaces, '+', $item, genDiff($value3, $value3, $level+1));
         }
     }, array_diff($contentKeys2, $contentKeys1));
 
     $result = implode("\n", array_merge($result1, $result2));
-    $spases2 = $spases - 2;
-    return  sprintf("%s\n%s\n%{$spases2}s", '{', $result, '}');
+    $spaces2 = $spaces - 2;
+    return  sprintf("%s\n%s\n%{$spaces2}s", '{', $result, '}');
 }
 
 function getContentForExt($file)
