7ffb6a5f771202bab0db7781905c90cc43623852
Santa Klaus <egorich2004@mail.ru>
2018-10-07 14:02:56 +0200

    fix json

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -4,11 +4,12 @@ namespace Formatters\Json;
 use function GenDiff\Differ\encode as encode;
 use function GenDiff\Differ\encode as stringify;
 
-/*
 function render($ast)
 {
     return json_encode($ast, JSON_PRETTY_PRINT);
-}*/
+}
+
+/*
 
 function render($ast, $spaces = '')
 {
@@ -20,13 +21,13 @@ function render($ast, $spaces = '')
         $node = array_map(function ($elem) use ($spaces, $type, $itemArr) {
             $quotes = true;
             $key = encode($elem, $quotes);
-            if ($elem ==='children' && $type == 'node') {
+            if ($elem == 'children' && $type == 'node') {
                 $tree = render($itemArr[$elem], "      {$spaces}");
                 return "    {$spaces}{$key}: {$tree}";
             }
             $value = stringify($itemArr[$elem], "    {$spaces}", $quotes);
-            if (($elem === 'newValue' && $type == 'added') ||
-                ($elem === 'oldValue' && $type == 'removed')) {
+            if (($elem == 'newValue' && $type == 'added') ||
+                ($elem == 'oldValue' && $type == 'removed')) {
                 return "    {$spaces}{$key}: {$value}";
             }
             return "    {$spaces}{$key}: {$value}";
@@ -39,3 +40,4 @@ function render($ast, $spaces = '')
     $strResult = implode(",\n", array_merge($result));
     return "[\n{$strResult}\n{$spaces}]";
 }
+*/
